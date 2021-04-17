5c6496fa3f970770b01f615e5c0af3d49025d4fa
nathanyee <hooticambo@gmail.com>
2021-04-17 15:39:58 -0400

    WebAssembly support

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,17 @@ pub mod lexer;
 pub mod object;
 pub mod parser;
 pub mod token;
+
+use object::environment;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn monkey_eval(input: &str) -> String {
+    let env = environment::Environment::new();
+    let program = ast::Program::new(input);
+    match evaluator::eval(program, env) {
+        Ok(o) => o.to_string(),
+        Err(e) => e.to_string(),
+    }
+}
